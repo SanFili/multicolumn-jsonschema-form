79dcf7c3f5188ac1f719cbe56ea3de5ed9676edd
Alexandra Filimonova <al.filimonova@vk.team>
2025-02-19 13:51:55 +0300

    fixes

--- src/components/ObjectFieldTemplate/ObjectFieldTemplate.tsx
+++ src/components/ObjectFieldTemplate/ObjectFieldTemplate.tsx
@@ -2,13 +2,16 @@ import React, { useState, useEffect, useMemo } from 'react';
 import { Typography, Col } from 'antd';
 import './ObjectFieldTemplate.css';
 
-type Test = {
+type PropertyType = {
   content: any;
   name: string;
 };
+
 function ObjectFieldTemplate(props: any) {
-  console.log(props);
-  const [properties, setProperties] = useState<Record<string, Test>>({});
+  // properties нужны нам для вывода в конце формы полей, которые мы могли забыть перечислить в layout
+  const [properties, setProperties] = useState<Record<string, PropertyType>>(
+    {}
+  );
   const layout = props.formContext.getLayout();
 
   useEffect(() => {
@@ -24,62 +27,67 @@ function ObjectFieldTemplate(props: any) {
 
   const gridLayout = useMemo(
     () =>
-      layout.sections.map((section: any) => {
-        return (
-          <div key={`section-${section.id}`}>
-            {section.header && (
-              <Typography.Title
-                level={section.header.heading_size || 4}
-                style={{
-                  textAlign: section.header.align || 'center',
-                }}
-              >
-                {section.header.title}
-              </Typography.Title>
-            )}
-            <div
-              className={
-                section.settings.layout
-                  ? `layout__section_${section.settings.layout}`
-                  : undefined
-              }
-            >
-              {section.blocks.map((block: any) => {
-                return (
-                  <div
-                    key={`${section.id}-${block.id}`}
-                    className="layout__block"
+      layout
+        ? layout.sections.map((section: any) => {
+            return (
+              <div key={section.id}>
+                {section.header && (
+                  <Typography.Title
+                    level={section.header.heading_size || 4}
                     style={{
-                      width: `${100 / (24 / block.width)}%`,
+                      textAlign: section.header.align || 'center',
                     }}
                   >
-                    {Object.keys(block.fields).map((el: any) => {
-                      const field = properties[el];
-                      delete properties[el];
-                      return field ? (
-                        <Col
-                          key={field.name}
-                          data-field={field.name}
-                          span={block.fields[el].width || 24 / block.columns}
-                          style={{ padding: '0 8px' }}
-                        >
-                          {field.content}
-                        </Col>
-                      ) : null;
-                    })}
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-        );
-      }),
+                    {section.header.title}
+                  </Typography.Title>
+                )}
+                <div className={`layout__section_${section.settings.layout}`}>
+                  {section.blocks.map((block: any) => {
+                    return (
+                      <div
+                        key={`${section.id}-${block.id}`}
+                        className="layout__block"
+                        style={{
+                          width: `${100 / (24 / block.width)}%`,
+                        }}
+                      >
+                        {Object.keys(block.fields).map((el: any) => {
+                          const field = properties[el];
+                          delete properties[el];
+                          return field ? (
+                            <Col
+                              key={field.name}
+                              data-field={field.name}
+                              span={block.fields[el].width || 24}
+                              style={{ padding: '0 8px' }}
+                            >
+                              {field.content}
+                            </Col>
+                          ) : null;
+                        })}
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            );
+          })
+        : null,
     [properties, layout]
   );
+
   return (
     <div>
-      <Typography.Title level={3}>{props.title}</Typography.Title>
+      {props.title ? (
+        <Typography.Title level={3}>{props.title}</Typography.Title>
+      ) : null}
+      {props.description ? (
+        <Typography.Text>{props.description}</Typography.Text>
+      ) : null}
+
       {gridLayout}
+
+      {/* поля, которые могли быть не указаны в layout */}
       {props.properties.map((el: any) =>
         properties[el.name] ? (
           <div key={el.name} style={{ padding: '0 8px' }}>
@@ -90,4 +98,5 @@ function ObjectFieldTemplate(props: any) {
     </div>
   );
 }
+
 export default ObjectFieldTemplate;
